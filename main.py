6af614039bcb53c24686c1543fb7a9362c16714a
Sayan Das <122361327+TechieSD100@users.noreply.github.com>
2025-08-11 00:39:43 +0530

    Update main.py

--- main.py
+++ main.py
@@ -1,5 +1,12 @@
+from flask import Flask
 import os
-from flask import Flask, request, jsonify
 
-port = int(os.environ.get("PORT", 8080))  # Railway sets PORT automatically
-app.run(host="0.0.0.0", port=port)
+app = Flask(__name__)
+
+@app.route("/")
+def home():
+    return "Hello from Railway!"
+
+if __name__ == "__main__":
+    port = int(os.environ.get("PORT", 8080))  # Railway will pass PORT env var
+    app.run(host="0.0.0.0", port=port)
